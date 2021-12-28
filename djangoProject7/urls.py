f4444e8cb10f76f2232a5c314a260a5a37e1fc38
Ahwireng Michael <43513906+FiifiQwontwo@users.noreply.github.com>
2021-12-28 05:47:10 +0000

    urls for static

--- djangoProject7/urls.py
+++ djangoProject7/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,3 +24,6 @@ urlpatterns = [
     path('member/', include('PCMember.urls')),
     path('attends/', include('PC_attend.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
