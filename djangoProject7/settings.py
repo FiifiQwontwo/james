578a1f310c486d3155fbb3f2ee35ed79dde0fb6f
Ahwireng Michael <43513906+FiifiQwontwo@users.noreply.github.com>
2022-01-05 10:06:09 +0000

    static files not working

--- djangoProject7/settings.py
+++ djangoProject7/settings.py
@@ -125,11 +125,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.User'
