26c1126ee682c80ad772efba82d6e0405b629430
Ahwireng Michael <43513906+FiifiQwontwo@users.noreply.github.com>
2022-01-14 17:46:43 +0000

    working on  detail pages for pcs names

--- Attendance/urls.py
+++ Attendance/urls.py
@@ -5,7 +5,7 @@ app_name = 'Attendance'
 urlpatterns = [
     path('', index, name='home page'),
     path('pcs/', list_pcs, name='list_pcs'),
-    path('pcs/<slug:slug>', pcs_detail, name='chapel head details'),
+    path('pcs/<slug:slug>', pcs_detail, name='pcadetails'),
     path('add/', create_pcs_name, name='add_new_pcs'),
     path('addpc/', create_name_pcs, name='add_pcs'),
 ]
