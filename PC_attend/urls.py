a699d32feaf7ba64f6a1581671a644f42f3151bb
Ahwireng Michael <43513906+FiifiQwontwo@users.noreply.github.com>
2022-01-15 19:06:04 +0000

    Done with Pc Memebers

--- PC_attend/urls.py
+++ PC_attend/urls.py
@@ -4,7 +4,7 @@ from PC_attend.views import attendance_list, attendance_detail, create_attendanc
 app_name = 'PC_attend'
 
 urlpatterns = [
-    path('list/', attendance_list, name='attendance list'),
-    path('list/<slug:slug>', attendance_detail, name='details_attend'),
-    path('create/', create_attendance, name='new attend')
+    path('Alist/', attendance_list, name='attendance_list'),
+    path('Alist/<slug:slug>', attendance_detail, name='details_attend'),
+    path('Acreate/', create_attendance, name='new_attend')
 ]
