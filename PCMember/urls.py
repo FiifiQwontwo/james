cfb2aa868806876ac1d9631eb7e192cc3c933ced
Ahwireng Michael <43513906+FiifiQwontwo@users.noreply.github.com>
2022-01-15 10:41:22 +0000

    working on  list  pages for pcs members

--- PCMember/urls.py
+++ PCMember/urls.py
@@ -4,7 +4,7 @@ from PCMember.views import create_pcmember, member_list, member_detail,Import_cs
 app_name = 'PCMember'
 urlpatterns = [
     path('list/', member_list, name='listmemeber'),
-    path('list/<slug:slug>', member_detail, name='member details'),
+    path('list/<slug:slug>', member_detail, name='member_details'),
     path('add/', create_pcmember, name='new member'),
     path('import_csv/', Import_csv, name="Import_csv"),
 ]
