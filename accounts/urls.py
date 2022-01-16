ed8bb1b7260e6fd179b648ef07e05ba38035a7f3
Ahwireng Michael <43513906+FiifiQwontwo@users.noreply.github.com>
2022-01-16 20:28:09 +0000

    Accounts urls creation

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,1 +1,9 @@
-from accounts.views import *
+from django.urls import path
+from accounts.views import login_user
+
+app_name = 'accounts'
+
+urlpatterns = [
+    path('login/', login_user, name='user_login'),
+
+]
